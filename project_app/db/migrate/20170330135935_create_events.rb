3518339c04a63d528a69dbfd17daa41e465bc8d3
Geny Cabral <genycabral@gmail.com>
2017-03-30 11:26:21 -0400

    setup

---
+++ project_app/db/migrate/20170330135935_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :type
+      t.string :category
+      t.datetime :event_date
+      t.string :image_url
+      t.string :event_url
+      t.float :price
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
+  end
+end
