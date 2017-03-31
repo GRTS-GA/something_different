035e5fed4a8fbf0d5d9ae83d7d581d44a196db5a
Geny Cabral <genycabral@gmail.com>
2017-03-31 12:17:36 -0400

    local update

--- project_app/app/controllers/events_controller.rb
+++ project_app/app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
 
-<<<<<<< HEAD
 
 # @ticket_list.parsed_response["_embedded"]["events"].first
 
@@ -12,20 +11,7 @@ end
 def show
  @event = Event.find(params[:id])
 end
-=======
-# @ticket_list.parsed_response["_embedded"]["events"].first
 
-  # before_action :set_event, only: [:show, :edit, :update, :destroy]
-  def index
-      @events = Event.all
-  end
->>>>>>> 165d57c043c2eecaf8f5a587b7096a1496259c7a
-
-  def show
-    @event = Event.find(params[:id])
-  end
-
-<<<<<<< HEAD
 def new
  @event = Event.create(params[:id])
 end
@@ -38,17 +24,7 @@ def renderEvent
   byebug
   @event = params[:id]
 end
-=======
->>>>>>> 165d57c043c2eecaf8f5a587b7096a1496259c7a
 
-  def new
-  # @event = Event.find(event_param)
-  end
-
-  def renderEvent
-    byebug
-    @event = params[:id]
-  end
 
 end
 
