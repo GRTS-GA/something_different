2f362f672ae768ebac83b671e56121a361df5d9e
Robert Romulus <rob@ovigz.com>
2017-04-01 20:36:39 -0400

    changes to permitter attributes in devise through the registration controller added picture_cache-

--- project_app/app/controllers/application_controller.rb
+++ project_app/app/controllers/application_controller.rb
@@ -23,4 +23,22 @@ class ApplicationController < ActionController::Base
       end
    end
 
+
+
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me, :picture, :picture_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :remember_me, :picture, :picture_cache])
+
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
+
+
+
+  end
 end
+
+
--- project_app/app/models/event.rb
+++ project_app/app/models/event.rb
@@ -5,4 +5,6 @@ class Event < ApplicationRecord
   mount_uploader :image, ImageUploader
 
 
+
 end
+
