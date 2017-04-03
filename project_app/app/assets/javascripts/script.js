ca6f5d065bfb971598cdfa551d7d3df2861c33ee
Samantha Garaicoa <s.garaicoa@gmail.com>
2017-04-03 09:11:08 -0400

    minor changes

--- project_app/app/assets/javascripts/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
- $("form").on('click',()=>{
-  console.log("hi")
-  function showDiv() {
-   document.getElementById('hidden').style.display = "flex";
-}
-showDiv()})
-});
-
